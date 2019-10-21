79cc526a07980a7dd4e2f1d54f18eddf8e43f468
Hiroaki Nakamura <hnakamur@gmail.com>
2019-10-22 05:39:59 +0900

    Add ErrorWithLV interface

--- lv.go
+++ lv.go
@@ -1,6 +1,38 @@
 package errstack
 
-import "errors"
+import (
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"strconv"
+	"time"
+)
+
+// ErrorWithLV is an error with methods for adding a pair of label and value.
+type ErrorWithLV interface {
+	error
+
+	String(label, value string) ErrorWithLV
+	Stringer(label string, value fmt.Stringer) ErrorWithLV
+	HexByte(label string, value byte) ErrorWithLV
+	HexBytes(label string, value []byte) ErrorWithLV
+	Fmt(label string, format string, a ...interface{}) ErrorWithLV
+	Bool(label string, value bool) ErrorWithLV
+	Int(label string, value int) ErrorWithLV
+	Int8(label string, value int8) ErrorWithLV
+	Int16(label string, value int16) ErrorWithLV
+	Int32(label string, value int32) ErrorWithLV
+	Int64(label string, value int64) ErrorWithLV
+	Uint(label string, value uint) ErrorWithLV
+	Uint8(label string, value uint8) ErrorWithLV
+	Uint16(label string, value uint16) ErrorWithLV
+	Uint32(label string, value uint32) ErrorWithLV
+	Uint64(label string, value uint64) ErrorWithLV
+	Float32(label string, value float32) ErrorWithLV
+	Float64(label string, value float64) ErrorWithLV
+	Time(label string, value time.Time, format string) ErrorWithLV
+	UTCTime(label string, value time.Time) ErrorWithLV
+}
 
 type errorWithLV struct {
 	err error
@@ -17,11 +49,11 @@ type errorWithLV struct {
 //
 // The pairs of labels and values can be get calling the LV
 // function to the wrapped error.
-func WithLV(err error, lv ...string) error {
+func WithLV(err error, lv ...string) ErrorWithLV {
 	if err == nil {
 		panic("err must not be nil")
 	}
-	if len(lv) == 0 || len(lv)%2 == 1 {
+	if len(lv)%2 == 1 {
 		panic("lv must be label and value pairs")
 	}
 
@@ -60,4 +92,107 @@ func (e *errorWithLV) Unwrap() error {
 
 func (e *errorWithLV) LV() []string {
 	return e.lv
+}
+
+func (e *errorWithLV) String(label, value string) ErrorWithLV {
+	e.lv = append(e.lv, label, value)
+	return e
+}
+
+func (e *errorWithLV) Stringer(label string, value fmt.Stringer) ErrorWithLV {
+	e.lv = append(e.lv, label, value.String())
+	return e
+}
+
+func (e *errorWithLV) HexByte(label string, value byte) ErrorWithLV {
+	e.lv = append(e.lv, label, hex.EncodeToString([]byte{value}))
+	return e
+}
+
+func (e *errorWithLV) HexBytes(label string, value []byte) ErrorWithLV {
+	e.lv = append(e.lv, label, hex.EncodeToString(value))
+	return e
+}
+
+func (e *errorWithLV) Fmt(label string, format string, a ...interface{}) ErrorWithLV {
+	e.lv = append(e.lv, label, fmt.Sprintf(format, a...))
+	return e
+}
+
+func (e *errorWithLV) Bool(label string, value bool) ErrorWithLV {
+	e.lv = append(e.lv, label, strconv.FormatBool(value))
+	return e
+}
+
+func (e *errorWithLV) Int(label string, value int) ErrorWithLV {
+	e.lv = append(e.lv, label, strconv.Itoa(value))
+	return e
+}
+
+func (e *errorWithLV) Int8(label string, value int8) ErrorWithLV {
+	e.lv = append(e.lv, label, strconv.Itoa(int(value)))
+	return e
+}
+
+func (e *errorWithLV) Int16(label string, value int16) ErrorWithLV {
+	e.lv = append(e.lv, label, strconv.Itoa(int(value)))
+	return e
+}
+
+func (e *errorWithLV) Int32(label string, value int32) ErrorWithLV {
+	e.lv = append(e.lv, label, strconv.FormatInt(int64(value), 10))
+	return e
+}
+
+func (e *errorWithLV) Int64(label string, value int64) ErrorWithLV {
+	e.lv = append(e.lv, label, strconv.FormatInt(int64(value), 10))
+	return e
+}
+
+func (e *errorWithLV) Uint(label string, value uint) ErrorWithLV {
+	e.lv = append(e.lv, label, strconv.FormatUint(uint64(value), 10))
+	return e
+}
+
+func (e *errorWithLV) Uint8(label string, value uint8) ErrorWithLV {
+	e.lv = append(e.lv, label, strconv.FormatUint(uint64(value), 10))
+	return e
+}
+
+func (e *errorWithLV) Uint16(label string, value uint16) ErrorWithLV {
+	e.lv = append(e.lv, label, strconv.FormatUint(uint64(value), 10))
+	return e
+}
+
+func (e *errorWithLV) Uint32(label string, value uint32) ErrorWithLV {
+	e.lv = append(e.lv, label, strconv.FormatUint(uint64(value), 10))
+	return e
+}
+
+func (e *errorWithLV) Uint64(label string, value uint64) ErrorWithLV {
+	e.lv = append(e.lv, label, strconv.FormatUint(uint64(value), 10))
+	return e
+}
+
+func (e *errorWithLV) Float32(label string, value float32) ErrorWithLV {
+	e.lv = append(e.lv, label, strconv.FormatFloat(float64(value), 'g', -1, 32))
+	return e
+}
+
+func (e *errorWithLV) Float64(label string, value float64) ErrorWithLV {
+	e.lv = append(e.lv, label, strconv.FormatFloat(value, 'g', -1, 64))
+	return e
+}
+
+func (e *errorWithLV) Time(label string, value time.Time, format string) ErrorWithLV {
+	if format == "" {
+		format = time.RFC3339
+	}
+	e.lv = append(e.lv, label, value.Format(format))
+	return e
+}
+
+func (e *errorWithLV) UTCTime(label string, value time.Time) ErrorWithLV {
+	e.lv = append(e.lv, label, value.Format("2006-01-02T15:04:05.999999Z"))
+	return e
 }
--- lv_test.go
+++ lv_test.go
@@ -3,8 +3,10 @@ package errstack_test
 import (
 	"errors"
 	"fmt"
+	"net"
 	"reflect"
 	"testing"
+	"time"
 
 	"github.com/hnakamur/errstack"
 )
@@ -104,6 +106,139 @@ func TestWithLV(t *testing.T) {
 		}
 		err := outer()
 		if got, want := errstack.LV(err), []string{"userID", "user1"}; !reflect.DeepEqual(got, want) {
+			t.Errorf("lv unmatch, got:%v, want:%v", got, want)
+		}
+	})
+}
+
+func TestErrorWithLV(t *testing.T) {
+	t.Run("String", func(t *testing.T) {
+		err := errstack.WithLV(errors.New("my error")).String("reqID", "req1")
+		if got, want := errstack.LV(err), []string{"reqID", "req1"}; !reflect.DeepEqual(got, want) {
+			t.Errorf("lv unmatch, got:%v, want:%v", got, want)
+		}
+	})
+	t.Run("Stringer", func(t *testing.T) {
+		err := errstack.WithLV(errors.New("my error")).Stringer("addr", net.IPv4(192, 0, 2, 1))
+		if got, want := errstack.LV(err), []string{"addr", "192.0.2.1"}; !reflect.DeepEqual(got, want) {
+			t.Errorf("lv unmatch, got:%v, want:%v", got, want)
+		}
+	})
+	t.Run("HexByte", func(t *testing.T) {
+		err := errstack.WithLV(errors.New("my error")).HexByte("byte", '\xba')
+		if got, want := errstack.LV(err), []string{"byte", "ba"}; !reflect.DeepEqual(got, want) {
+			t.Errorf("lv unmatch, got:%v, want:%v", got, want)
+		}
+	})
+	t.Run("HexBytes", func(t *testing.T) {
+		err := errstack.WithLV(errors.New("my error")).HexBytes("bytes", []byte{'\xba', '\xbe'})
+		if got, want := errstack.LV(err), []string{"bytes", "babe"}; !reflect.DeepEqual(got, want) {
+			t.Errorf("lv unmatch, got:%v, want:%v", got, want)
+		}
+	})
+	t.Run("Fmt", func(t *testing.T) {
+		err := errstack.WithLV(errors.New("my error")).Fmt("strings", "%v", []string{"a", "b"})
+		if got, want := errstack.LV(err), []string{"strings", "[a b]"}; !reflect.DeepEqual(got, want) {
+			t.Errorf("lv unmatch, got:%v, want:%v", got, want)
+		}
+	})
+	t.Run("Bool", func(t *testing.T) {
+		err := errstack.WithLV(errors.New("my error")).Bool("bool", true)
+		if got, want := errstack.LV(err), []string{"bool", "true"}; !reflect.DeepEqual(got, want) {
+			t.Errorf("lv unmatch, got:%v, want:%v", got, want)
+		}
+	})
+	t.Run("Int", func(t *testing.T) {
+		err := errstack.WithLV(errors.New("my error")).Int("int", -12)
+		if got, want := errstack.LV(err), []string{"int", "-12"}; !reflect.DeepEqual(got, want) {
+			t.Errorf("lv unmatch, got:%v, want:%v", got, want)
+		}
+	})
+	t.Run("Int8", func(t *testing.T) {
+		err := errstack.WithLV(errors.New("my error")).Int8("int8", -12)
+		if got, want := errstack.LV(err), []string{"int8", "-12"}; !reflect.DeepEqual(got, want) {
+			t.Errorf("lv unmatch, got:%v, want:%v", got, want)
+		}
+	})
+	t.Run("Int16", func(t *testing.T) {
+		err := errstack.WithLV(errors.New("my error")).Int16("int16", -12)
+		if got, want := errstack.LV(err), []string{"int16", "-12"}; !reflect.DeepEqual(got, want) {
+			t.Errorf("lv unmatch, got:%v, want:%v", got, want)
+		}
+	})
+	t.Run("Int32", func(t *testing.T) {
+		err := errstack.WithLV(errors.New("my error")).Int32("int32", -12)
+		if got, want := errstack.LV(err), []string{"int32", "-12"}; !reflect.DeepEqual(got, want) {
+			t.Errorf("lv unmatch, got:%v, want:%v", got, want)
+		}
+	})
+	t.Run("Int64", func(t *testing.T) {
+		err := errstack.WithLV(errors.New("my error")).Int64("int64", -12)
+		if got, want := errstack.LV(err), []string{"int64", "-12"}; !reflect.DeepEqual(got, want) {
+			t.Errorf("lv unmatch, got:%v, want:%v", got, want)
+		}
+	})
+	t.Run("Uint", func(t *testing.T) {
+		err := errstack.WithLV(errors.New("my error")).Uint("uint", 12)
+		if got, want := errstack.LV(err), []string{"uint", "12"}; !reflect.DeepEqual(got, want) {
+			t.Errorf("lv unmatch, got:%v, want:%v", got, want)
+		}
+	})
+	t.Run("Uint8", func(t *testing.T) {
+		err := errstack.WithLV(errors.New("my error")).Uint8("uint8", 12)
+		if got, want := errstack.LV(err), []string{"uint8", "12"}; !reflect.DeepEqual(got, want) {
+			t.Errorf("lv unmatch, got:%v, want:%v", got, want)
+		}
+	})
+	t.Run("Uint16", func(t *testing.T) {
+		err := errstack.WithLV(errors.New("my error")).Uint16("uint16", 12)
+		if got, want := errstack.LV(err), []string{"uint16", "12"}; !reflect.DeepEqual(got, want) {
+			t.Errorf("lv unmatch, got:%v, want:%v", got, want)
+		}
+	})
+	t.Run("Uint32", func(t *testing.T) {
+		err := errstack.WithLV(errors.New("my error")).Uint32("uint32", 12)
+		if got, want := errstack.LV(err), []string{"uint32", "12"}; !reflect.DeepEqual(got, want) {
+			t.Errorf("lv unmatch, got:%v, want:%v", got, want)
+		}
+	})
+	t.Run("Uint64", func(t *testing.T) {
+		err := errstack.WithLV(errors.New("my error")).Uint64("uint64", 12)
+		if got, want := errstack.LV(err), []string{"uint64", "12"}; !reflect.DeepEqual(got, want) {
+			t.Errorf("lv unmatch, got:%v, want:%v", got, want)
+		}
+	})
+	t.Run("Float32", func(t *testing.T) {
+		err := errstack.WithLV(errors.New("my error")).Float32("float32", 1.2)
+		if got, want := errstack.LV(err), []string{"float32", "1.2"}; !reflect.DeepEqual(got, want) {
+			t.Errorf("lv unmatch, got:%v, want:%v", got, want)
+		}
+	})
+	t.Run("Float64", func(t *testing.T) {
+		err := errstack.WithLV(errors.New("my error")).Float64("float64", 1.2)
+		if got, want := errstack.LV(err), []string{"float64", "1.2"}; !reflect.DeepEqual(got, want) {
+			t.Errorf("lv unmatch, got:%v, want:%v", got, want)
+		}
+	})
+	t.Run("Time", func(t *testing.T) {
+		loc, err := time.LoadLocation("Asia/Tokyo")
+		if err != nil {
+			t.Fatal(err)
+		}
+		err = errstack.WithLV(errors.New("my error")).Time("time", time.Date(2019, 10, 22, 5, 31, 53, 123456789, loc), time.RFC3339Nano)
+		if got, want := errstack.LV(err), []string{"time", "2019-10-22T05:31:53.123456789+09:00"}; !reflect.DeepEqual(got, want) {
+			t.Errorf("lv unmatch, got:%v, want:%v", got, want)
+		}
+	})
+	t.Run("UTCTime", func(t *testing.T) {
+		err := errstack.WithLV(errors.New("my error")).UTCTime("utctime", time.Date(2019, 10, 22, 5, 31, 53, 123456789, time.UTC))
+		if got, want := errstack.LV(err), []string{"utctime", "2019-10-22T05:31:53.123456Z"}; !reflect.DeepEqual(got, want) {
+			t.Errorf("lv unmatch, got:%v, want:%v", got, want)
+		}
+	})
+	t.Run("StringInt64", func(t *testing.T) {
+		err := errstack.WithLV(errors.New("my error")).String("reqID", "req1").Int64("userID", 1)
+		if got, want := errstack.LV(err), []string{"reqID", "req1", "userID", "1"}; !reflect.DeepEqual(got, want) {
 			t.Errorf("lv unmatch, got:%v, want:%v", got, want)
 		}
 	})
